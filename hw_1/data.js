cc70e26d4ac093b2da7486d70821606993335181
popov_oleg <jim_power@mail.ru>
2024-08-21 19:46:36 +0300

    hw_1

---
+++ hw_1/data.js
@@ -0,0 +1,66 @@
+export const workouts = `
+[
+    {
+        "id": "1",
+        "name_of_workout": "Yoga",
+        "time_of_workout": "09:30 - 11:00",
+        "max_number_of_participants": "20",
+        "current_number_of_participants": "10"
+    },
+    {
+        "id": "2",
+        "name_of_workout": "Aqua Cardio",
+        "time_of_workout": "11:00 - 11:45",
+        "max_number_of_participants": "12",
+        "current_number_of_participants": "4"
+    },
+    {
+        "id": "3",
+        "name_of_workout": "Pilates",
+        "time_of_workout": "11:00 - 11:50",
+        "max_number_of_participants": "15",
+        "current_number_of_participants": "10"
+    },
+    {
+        "id": "4",
+        "name_of_workout": "Step",
+        "time_of_workout": "10:00 - 10:55",
+        "max_number_of_participants": "25",
+        "current_number_of_participants": "20"
+    },
+    {
+        "id": "5",
+        "name_of_workout": "Aqua Dumbells",
+        "time_of_workout": "11:00 - 11:45",
+        "max_number_of_participants": "10",
+        "current_number_of_participants": "8"
+    },
+    {
+        "id": "6",
+        "name_of_workout": "Legs & Buts",
+        "time_of_workout": "10:00 - 10:55",
+        "max_number_of_participants": "20",
+        "current_number_of_participants": "20"
+    },
+    {
+        "id": "7",
+        "name_of_workout": "Body balance",
+        "time_of_workout": "09:30 - 11:00",
+        "max_number_of_participants": "20",
+        "current_number_of_participants": "13"
+    },
+    {
+        "id": "8",
+        "name_of_workout": "Super press",
+        "time_of_workout": "09:00 - 09:55",
+        "max_number_of_participants": "25",
+        "current_number_of_participants": "7"
+    },
+    {
+        "id": "9",
+        "name_of_workout": "Body Sculpt",
+        "time_of_workout": "11:30 - 12:00",
+        "max_number_of_participants": "15",
+        "current_number_of_participants": "5"
+    }
+]`;
